e1b0c8e54f49a9f1e2a2362d48c0fa85356fbeeb
Alex Gooding <agooding@illumina.com>
2022-04-08 12:10:47 +0100

    initial timer work

--- break_timer/static/break_timer/break-timer.js
+++ break_timer/static/break_timer/break-timer.js
@@ -5,9 +5,36 @@ function toastControl(time, toastToTriggerId, toastToDismissId=null) {
     }
     var toastToTrigger = document.getElementById(toastToTriggerId);
     var timeInMilliseconds = minutesToMilliseconds(time);
+    countdownTimer(timeInMilliseconds);
     setTimeout(function(){ toastToTrigger.classList.add("show"); }, timeInMilliseconds);
 }
 
 function minutesToMilliseconds(minutes) {
     return minutes * 1000 * 60;
+}
+
+function countdownTimer(milliseconds) {
+    var timerElement = document.getElementById("timer");
+    var timeLeft = milliseconds;
+    timerElement.innerHTML = formatMillisecondsToTime(timeLeft);
+    var interval = setInterval(function() {
+        timeLeft -= 1000;
+        timerElement.innerHTML = formatMillisecondsToTime(timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+        }  
+    }, 1000);
+}
+
+function formatMillisecondsToTime(milliseconds) {
+    var totalSeconds = milliseconds / 1000;
+    var seconds = totalSeconds % 60;
+    var minutes = (totalSeconds - seconds) / 60;
+    var hours = (minutes - (minutes % 60)) / 60;
+
+    return formatTimeComponent(hours) + ":" + formatTimeComponent(minutes) + ":" + formatTimeComponent(seconds)
+}
+
+function formatTimeComponent(value) {
+    return ("0" + value).slice(-2);
 }
