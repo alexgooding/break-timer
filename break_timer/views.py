52e3a4f798f166bb4c7af621855fe5f1b4da63e4
Alex Gooding <alexxgooding@gmail.com>
2023-02-24 17:28:49 +0000

    use a user based model for storing timer times

--- break_timer/views.py
+++ break_timer/views.py
@@ -1,36 +1,40 @@
-from django.shortcuts import render
 from break_timer.utils import convert_to_float, round_to_nearest_second, format_snooze_length
-from django.db import transaction
-from break_timer.models import MuteAudio
-from django.http import HttpResponse
-from django.views.generic import View, TemplateView, FormView
+from break_timer.models import Timer
+from break_timer.forms import TimerForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import View, TemplateView, CreateView
 
-class HomeView(TemplateView):
+class HomeView(CreateView):
     template_name = 'home.html'
+    model = Timer
+    form_class = TimerForm
+    success_url = '/timer/'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 class TimerView(TemplateView):
     template_name = 'timer.html'
     
-    def post(self, request):
-        work_length = convert_to_float(request.POST['work-length'])
-        break_length = convert_to_float(request.POST['break-length'])
-        rounded_work_length = round_to_nearest_second(work_length)
-        rounded_break_length = round_to_nearest_second(break_length)
-        rounded_work_snooze_length = round_to_nearest_second(work_length/5)
-        rounded_break_snooze_length = round_to_nearest_second(break_length/5)
-        formatted_work_snooze_length = format_snooze_length(rounded_work_snooze_length)
-        formatted_break_snooze_length = format_snooze_length(rounded_break_snooze_length)
+    latest_timer = Timer.objects.latest('created')
+    rounded_work_length = round_to_nearest_second(latest_timer.work_length)
+    rounded_break_length = round_to_nearest_second(latest_timer.break_length)
+    rounded_work_snooze_length = round_to_nearest_second(latest_timer.work_length/5)
+    rounded_break_snooze_length = round_to_nearest_second(latest_timer.break_length/5)
+    formatted_work_snooze_length = format_snooze_length(rounded_work_snooze_length)
+    formatted_break_snooze_length = format_snooze_length(rounded_break_snooze_length)
 
-        data = {
-            'work_length': rounded_work_length,
-            'break_length': rounded_break_length,
-            'work_snooze_length': rounded_work_snooze_length,
-            'break_snooze_length': rounded_break_snooze_length,
-            'formatted_work_snooze_length': formatted_work_snooze_length,
-            'formatted_break_snooze_length': formatted_break_snooze_length
-        }
-
-        return self.render_to_response(data)
+    extra_context = {
+        'work_length': rounded_work_length,
+        'break_length': rounded_break_length,
+        'work_snooze_length': rounded_work_snooze_length,
+        'break_snooze_length': rounded_break_snooze_length,
+        'formatted_work_snooze_length': formatted_work_snooze_length,
+        'formatted_break_snooze_length': formatted_break_snooze_length
+    }   
 
 class ToggleMuteView(View):
     def post(self, request):
