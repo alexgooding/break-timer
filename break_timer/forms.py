0a4c7db29c3c746a94dd88e6f232236d654c18ad
Alex Gooding <alexxgooding@gmail.com>
2023-02-27 00:35:27 +0000

    use boostrap styling for all pages

--- break_timer/forms.py
+++ break_timer/forms.py
@@ -10,11 +10,11 @@ class TimerForm(forms.ModelForm):
     def clean_work_length(self):
         work_length = self.cleaned_data['work_length']
         if work_length <= 0.0 or work_length >= 6000.0:
-            raise ValidationError('Work length value must be greater than 0 and less than 6000')
+            raise ValidationError('Work length value must be greater than 0 and less than 6000!')
         return work_length
     
     def clean_break_length(self):
         break_length = self.cleaned_data['break_length']
         if break_length <= 0.0 or break_length >= 6000.0:
-            raise ValidationError('Break length value must be greater than 0 and less than 6000')
+            raise ValidationError('Break length value must be greater than 0 and less than 6000!')
         return break_length
